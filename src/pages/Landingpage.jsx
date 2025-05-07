69e42f5c0a16de7d22d21c9c77ffcc2aa169ce95
unknown <beyasabellach@gmail.com>
2025-05-07 18:21:09 +0800

    added shes starfish

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '../components/ui/carousel'
+import { toast } from 'sonner';
+import axios from 'axios';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Star } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
 
 function Landingpage() {
+  const [feedback, setFeedback] = useState([]);
+
+  const getFeedbacks = async () => {
+    try {
+      const url = localStorage.getItem('url') + "customer.php";
+      const formData = new FormData();
+      formData.append("operation", "getFeedbacks");
+      const res = await axios.post(url, formData);
+      setFeedback(res.data !== 0 ? res.data : []);
+      console.log("res ni getFeedbacks", res);
+
+
+
+
+    } catch (error) {
+      toast.error("Something went wrong");
+      console.error(error);
+
+    }
+  }
+  useEffect(() => {
+    getFeedbacks();
+  }, []);
   return (
     <div className="scroll-smooth">
 
@@ -21,7 +50,7 @@ function Landingpage() {
 
       {/* Section 3  */}
       <section className="flex items-center justify-center h-screen bg-white">
-     
+
 
         <div className="w-full max-w-4xl">
           <Carousel>
@@ -46,6 +75,116 @@ function Landingpage() {
             <CarouselPrevious />
             <CarouselNext />
           </Carousel>
+        </div>
+      </section>
+      <section className="flex items-center justify-center h-screen bg-white">
+        <div>
+          <h1 className="text-3xl font-bold mb-4">Feedbacks</h1>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {feedback.map((item, index) => (
+              <div key={index}>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>{item.customer_fullname}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className=" border-2 border-gray-400 rounded-lg">
+                      <p className="text-sm p-3" >{item.customersreviews}</p>
+                    </div>
+                    <div className="mt-2">
+                      <div className="flex">
+                      <p> <span>🤝</span>Hospitality: </p>
+                      <div className="flex ml-7">
+                      {[1, 2, 3, 4, 5].map((starValue) => (
+                          <Star
+                            key={starValue}
+                            className={cn(
+                              "h-6 w-6 cursor-pointer transition-colors",
+                              starValue <= item.customersreviews_hospitality_rate ? "fill-yellow-400 stroke-yellow-400" : "stroke-gray-400"
+                            )}
+                          />
+                        ))}
+                      </div>
+                    
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                    <div className="flex">
+                      <p> <span>🧹</span>Cleanliness: </p>
+                      <div className="flex ml-6">
+                      {[1, 2, 3, 4, 5].map((starValue) => (
+                          <Star
+                            key={starValue}
+                            className={cn(
+                              "h-6 w-6 cursor-pointer transition-colors",
+                              starValue <= item.customersreviews_cleanliness_rate ? "fill-yellow-400 stroke-yellow-400" : "stroke-gray-400"
+                            )}
+                          />
+                        ))}
+                      </div>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                    <div className="flex">
+                      <p><span>😊</span>Behavior: </p>
+                      <div className="flex ml-10">
+                      {[1, 2, 3, 4, 5].map((starValue) => (
+                          <Star
+                            key={starValue}
+                            className={cn(
+                              "h-6 w-6 cursor-pointer transition-colors",
+                              starValue <= item.customersreviews_behavior_rate ? "fill-yellow-400 stroke-yellow-400" : "stroke-gray-400"
+                            )}
+                          />
+                        ))}
+                      </div>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                    <div className="flex">
+                      <p><span>🏢</span>Facilities: </p>
+                      <div className="flex ml-11">
+                      {[1, 2, 3, 4, 5].map((starValue) => (
+                          <Star
+                            key={starValue}
+                            className={cn(
+                              "h-6 w-6 cursor-pointer transition-colors",
+                              starValue <= item.customersreviews_facilities_rate ? "fill-yellow-400 stroke-yellow-400" : "stroke-gray-400"
+                            )}
+                          />
+                        ))}
+                      </div>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                    <div className="flex">
+                      <p><span>🍽️</span>Food: </p>
+                      <div className="flex ml-16">
+                      {[1, 2, 3, 4, 5].map((starValue) => (
+                          <Star
+                            key={starValue}
+                            className={cn(
+                              "h-6 w-6 cursor-pointer transition-colors",
+                              starValue <= item.customersreviews_foods_rate ? "fill-yellow-400 stroke-yellow-400" : "stroke-gray-400"
+                            )}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                    </div>
+
+
+
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
+
+          </div>
+
+
+
+
         </div>
       </section>
 
